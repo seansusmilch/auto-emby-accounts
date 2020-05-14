81558de8b1fbde08804c67347af59d3555ec9a27
Sean Susmilch <seantsusmilch@gmail.com>
2020-05-13 21:30:36 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# auto-emby-accounts
-This script will make adding emby accounts with specified settings super quick
+# Auto Emby Accounts
+
+Use python requests to add and configure new accounts on an Emby server.
+
+## Notice
+
+>I'm an inexperienced coder and even more inexperienced when it comes to API requests. Use this script at your own risk.
+
+## Requirements
+
+* Python (I've only tested 3.8.2) with the following modules
+  * requests
+  * json
+
+## Usage
+
+This script takes action on 3 files `in.txt`,`out.txt`, and `secrets.py` which need to be in the same directory as `auto-emby-accts.py`.
+
+To define your server url and api token, make a file named `secrets.py` in the same directory as `auto-emby-accts.py` with this format
+
+```python
+base_url = ""   # put the url your emby server here (Ex: "http://localhost:8096")
+api_token = ""  # put the static api token for your server here (Ex: "ac4e8d00c23842f39e6f793383152360")
+```
+
+To define the usernames of the accounts, create a file named `in.txt` and use the following format, values comma separated.
+
+```text
+test, testConnectUsername
+testConnectUsername
+```
+
+ With the first line in `in.txt`, the script will create a user named **test** and attempt to link **testConnectUsername** to that account.
+
+The second line will create a user named **!testConnectUsername** (notice the "!" at the beginning) and attempt to link **testConnectUsername** to that account.
+
+The account password will be a random 20 character alphanumeric string.
+
+## Output
+
+Outupt of this script, in `out.txt`, should look something like this
+
+```text
+!test	testConnectUsername K46uLsbhQ6EhZY2a7OK9
+test2	testConnectUsername2    LCM4Pyl2AFc7gr8zQwi4	-CONNECTERROR-
+-----------------------------
+```
+
+Values are tab separated. The leftmost value is the username *on the emby server*, while the second value is the *emby connect username*. The third value is the password for the account on the server. Any values past this should be error messages. A line of dashes will print after each run, given that no uncaught exceptions occured.
+
+## Errors
+
+I have not extensively tested for every possible error but here are some that can occur.
+
+```text
+-CONNECTERROR-                              out.txt error. The script could not link an account to emby connect. The account may be already linked on the server, or the connect username is invalid.
+-CREATEERROR-                               out.txt error. This shouldn't be able to be written to out.txt
+...already exists! Skipping...              Console error. There is an account on the emby server that has the username trying to be added.
+API token is invalid. Stopping...           Console error. Your API token in secrets.py is invalid.
+```
+
+### Emby User Settings
+
+Here's a list of user settings explicitly changed by this script. A screenshot of what the user settings page will look like is also down there. These settings are to suit my own needs but can be edited in `auto-emby-accts.py` under the section that begins with the comment `# CHANGE POLICY`
+
+|   Feature    |   Value    |
+|-------|-------|
+|IsAdministrator|False|
+|IsHidden|True|
+|IsHiddenRemotely|True|
+|IsDisabled|False|
+|EnableRemoteControlOfOtherUsers|False|
+|EnableSharedDeviceControl|False|
+|EnableRemoteAccess|True|
+|EnableLiveTvManagement|False|
+|EnableLiveTvAccess|False|
+|EnableContentDeletion|False|
+|EnableContentDownloading|True|
+|EnableSubtitleDownloading|False|
+|EnableSubtitleManagement|False|
+|EnableSyncTranscoding|False|
+|EnableMediaConversion|False|
+|EnablePublicSharing|False|
+
+![alt text](https://i.imgur.com/uYbYqMk.png)
---
+++ auto-emby-accts.py
@@ -0,0 +1,194 @@
+import requests
+import json
+from os import path
+import random
+import string
+from secrets import base_url, api_token
+
+def add_user(username, passwd, connect, base_url, api_token):
+    '''
+    username, the username for the new user
+    passwd, the password for the new user
+    connect, the emby connect account username/email
+    base_url, the direct url to the emby server
+    api_token, static api token for the emby server
+    '''
+
+    api_json = {"X-Emby-Token": {api_token}}
+    headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"}
+    msg = ""
+
+    #
+    # CREATE NEW USER
+    #
+
+    raw_data = {
+        'name': username
+    }
+    raw_data.update(api_json)
+
+    res = requests.post(base_url+'/Users/New', params=raw_data, headers=headers)
+    if res.status_code > 300:
+        msg += "-CREATEERROR-"
+    try:
+        data = json.loads(res.text)
+    except json.decoder.JSONDecodeError as e:
+        if ' already exists' in res.text:
+            print(username, ' already exists! Skipping...')
+        elif 'Access token is invalid or expired.' == res.text:
+            print("API token is invalid. Stopping...")
+        else:
+            print('error: ' + res.text)
+            print(username, ' unknown error when creating! Skipping...')
+        return
+    newId = data['Id']
+    print(username, ' create user ', res.status_code)
+
+    #
+    # UPDATE PASSWORD
+    # 
+
+    device = {
+        'X-Emby-Client': "Requests Python",
+        'X-Emby-Device-Name': "Python",
+        "X-Emby-Client-Version": "4.4.2.0",
+        "X-Emby-Device-Id": "12345678-1234-1234-1234-123456789012"
+    }
+    raw_data = {
+        "username": username,
+        "Pw": ""
+    }
+    raw_data.update(device)
+    res = requests.post(base_url+'/Users/AuthenticateByName', params=raw_data, headers=headers)
+    if res.status_code > 300:
+        msg += "-AUTHERROR-"
+    
+    data = json.loads(res.text)
+    temp_token = data['AccessToken']
+    # need access token to use in place of api token because static api tokens cannot change user passwords
+    raw_data = {
+        "X-Emby-Token": temp_token,
+        "CurrentPw": "",
+        "NewPw": passwd
+    }
+    raw_data.update(device)
+    res = requests.post(base_url+'/emby/Users/'+newId+'/Password', params=raw_data, headers=headers)
+    print(username, ' add password ', passwd, ' ', res.status_code)
+    if res.status_code > 300:
+        msg += "-PASSWORDERROR-"
+
+    #
+    # CHANGE POLICY
+    # 
+
+    raw_data = {
+        "IsAdministrator": False,
+        "IsHidden": True,
+        "IsHiddenRemotely": True,
+        "IsDisabled": False,
+        # MaxParentalRating	integer($int32, nullable)
+        # BlockedTags	[string]
+        # EnableUserPreferenceAccess	boolean
+        # AccessSchedules	[Configuration.AccessSchedule{...}]
+        # BlockUnratedItems	[string($enum)(Movie, Trailer, Series, Music, Game, Book, LiveTvChannel, LiveTvProgram, ChannelContent, Other)]
+        "EnableRemoteControlOfOtherUsers": False,
+        "EnableSharedDeviceControl": False,
+        "EnableRemoteAccess": True,
+        "EnableLiveTvManagement": False,
+        "EnableLiveTvAccess": False,
+        # EnableMediaPlayback	boolean
+        # EnableAudioPlaybackTranscoding	boolean
+        # EnableVideoPlaybackTranscoding	boolean
+        # EnablePlaybackRemuxing	boolean
+        "EnableContentDeletion": False,
+        # EnableContentDeletionFromFolders	[string]
+        "EnableContentDownloading": True,
+        "EnableSubtitleDownloading": False,
+        "EnableSubtitleManagement": False,
+        "EnableSyncTranscoding": False,
+        "EnableMediaConversion": False,
+        # EnabledDevices	[string]
+        # EnableAllDevices	boolean
+        # EnabledChannels	[string]
+        # EnableAllChannels	boolean
+        # "EnabledFolders": "",
+        # "EnableAllFolders": False,
+        # InvalidLoginAttemptCount	integer($int32)
+        "EnablePublicSharing": False,
+        # "BlockedMediaFolders": "",
+        # BlockedChannels	[string]
+        # RemoteClientBitrateLimit	integer($int32)
+        # AuthenticationProviderId	string
+        # ExcludedSubFolders	[string]
+        # DisablePremiumFeatures	boolean
+    }
+    raw_data.update(api_json)
+    res = requests.post(base_url+'/emby/Users/'+newId+'/Policy', params=raw_data, headers=headers)
+    print(username, ' update policy ', res.status_code)
+    if res.status_code > 300:
+        msg += "-POLICYERROR-"
+
+    #
+    # ADD EMBY CONNECT ACCOUNT
+    #
+
+    raw_data = {
+        "ConnectUsername": connect
+    }
+    raw_data.update(api_json)
+    res = requests.post(base_url+'/emby/Users/'+newId+'/Connect/Link', params=raw_data, headers=headers)
+    print(username, ' add emby connect ', res.status_code)
+    if res.status_code > 300:
+        msg += "-CONNECTERROR-"
+        
+
+    #
+    # APPEND TO OUT.TXT
+    # 
+
+    with open(dir + '/out.txt', 'a+', encoding='utf-8') as outfile:
+        outfile.seek(0)
+        if len(outfile.read(10)) > 0:
+            outfile.write('\n')
+        outfile.write(username + '\t' + connect + '\t' + passwd + ('\t'+msg if msg else ''))
+        outfile.close()
+
+
+
+
+
+# get users from in.txt
+
+dir = path.split(path.abspath(__file__))
+dir = dir[0]
+
+newUsrs = []
+with open(dir + '/in.txt', 'a+') as intxt:
+    print('Getting new users from\n' + dir + '/in.txt')
+    intxt.seek(0)
+    for line in intxt.readlines():
+        newUsrs.append(line.rstrip().split(','))
+    intxt.close()
+
+# run the add_user function for each new user with a password
+
+newUsrName = connect = ""
+for user in newUsrs:
+
+    # new passwd
+    passwd = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(20))
+    print(passwd)
+
+    if len(user) == 1:
+        connect = user[0]
+        username = '!' + user[0]
+    elif len(user) == 2:
+        connect = user[1]
+        username = user[0]
+    else:
+        print('yo shit in the wrong format (in.txt)')
+    add_user(username, passwd, connect, base_url, api_token)
+
+with open(dir + '/out.txt', 'a+', encoding='utf-8') as outfile:
+    outfile.write('\n-----------------------------')
+    outfile.close()
